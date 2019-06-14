85af56e6f3ac5b8360d316e1b4b2a71bf32c6b43
Boris Victor <boristhevictor@gmail.com>
2019-06-14 09:34:27 +0200

    Created the most basic project structure. Closes #1

---
+++ src/test/java/com/borysp/payments/uma/uma/UmaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.borysp.payments.uma.uma;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UmaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
