89e1c1c3e08355740e5c0851672ff4bff6fd70a7
Boris Victor <boristhevictor@gmail.com>
2019-06-14 10:03:01 +0200

    added base tests

---
+++ src/main/java/com/borysp/payments/uma/app/model/Employee.java
@@ -0,0 +1,7 @@
+package com.borysp.payments.uma.app.model;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+}
---
+++ src/test/java/com/borysp/payments/uma/app/model/EmployeeTest.java
@@ -0,0 +1,46 @@
+package com.borysp.payments.uma.app.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class EmployeeTest {
+
+    @Test
+    public void representsAnEmployee() {
+        Employee testEmployee = getTestEmployee(1, 0, "Cannon", "Fodder");
+        Assertions.assertThat(testEmployee)
+                .hasFieldOrPropertyWithValue("id", null)
+                .hasFieldOrPropertyWithValue("grade", 1)
+                .hasFieldOrPropertyWithValue("salary", 0)
+                .hasFieldOrPropertyWithValue("name", "Cannon")
+                .hasFieldOrPropertyWithValue("surname", "Fodder");
+    }
+
+    private Employee getTestEmployee(int grade, int salary, String name, String surname) {
+        return new Employee();
+    }
+
+    @Test
+    public void equalsWorks() {
+        Assertions.assertThat(getTestEmployee(1,1, "Mark", "Twain"))
+            .isEqualTo(getTestEmployee(1,1, "Mark", "Twain"));
+    }
+
+    @Test
+    public void hashCodeWorks() {
+        Assertions.assertThat(getTestEmployee(1,1, "Mark", "Twain").hashCode())
+                .isEqualTo(getTestEmployee(1,1, "Mark", "Twain").hashCode());
+    }
+
+    @Test
+    public void toStringWorks() {
+        Assertions.assertThat(getTestEmployee(1, 1, "Mark", "Twain").toString())
+                .contains("Mark")
+                .contains("Twain")
+                .contains("salary")
+                .contains("grade")
+                .contains("name")
+                .contains("surname");
+    }
+
+}
