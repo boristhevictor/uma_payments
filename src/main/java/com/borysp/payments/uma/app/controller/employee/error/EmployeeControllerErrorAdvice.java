79d5be151492306cdd058e23bef1b50aaf46bd18
Boris Victor <boristhevictor@gmail.com>
2019-06-14 17:27:52 +0200

    #21 Oops. Fixing copy paste error

--- src/main/java/com/borysp/payments/uma/app/controller/employee/error/EmployeeControllerErrorAdvice.java
+++ src/main/java/com/borysp/payments/uma/app/controller/employee/error/EmployeeControllerErrorAdvice.java
@@ -5,16 +5,14 @@ import lombok.Setter;
 import lombok.experimental.Accessors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
-@RestController
-@RequestMapping("/employee")
+@ControllerAdvice
 public class EmployeeControllerErrorAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EmployeeNotFoundException.class)
