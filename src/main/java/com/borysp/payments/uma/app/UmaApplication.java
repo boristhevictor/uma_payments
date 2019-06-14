28a50609b2ae2d202d97d411b27688894a1061b9
Boris Victor <boristhevictor@gmail.com>
2019-06-14 09:47:33 +0200

    pkg name changed

--- src/main/java/com/borysp/payments/uma/app/UmaApplication.java
+++ src/main/java/com/borysp/payments/uma/app/UmaApplication.java
@@ -1,4 +1,4 @@
-package com.borysp.payments.uma.uma;
+package com.borysp.payments.uma.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/borysp/payments/uma/app/UmaApplicationTests.java
+++ src/test/java/com/borysp/payments/uma/app/UmaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.borysp.payments.uma.uma;
+package com.borysp.payments.uma.app;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
