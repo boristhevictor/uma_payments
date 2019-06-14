2519cdd4015f15227003c162c452a1416ea3dfc3
Boris Victor <boristhevictor@gmail.com>
2019-06-14 11:54:41 +0200

    Implemented the service CRUD methods, extended and fixed test failures. Closes #4

--- src/main/java/com/borysp/payments/uma/app/service/employee/crud/EmployeeService.java
+++ src/main/java/com/borysp/payments/uma/app/service/employee/crud/EmployeeService.java
@@ -7,6 +7,10 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+import static java.util.Objects.*;
+
 @Service
 @Primary
 public class EmployeeService implements EmployeeCRUDService {
@@ -18,23 +22,45 @@ public class EmployeeService implements EmployeeCRUDService {
     }
 
     @Override
-    public Employee fetch(Integer id) {
-        return null;
+    public Optional<Employee> fetch(Integer id) {
+        if(id==null) {
+            return Optional.empty();
+        }
+        return employeeRepository.findById(id);
     }
 
     @Override
     public Employee save(Employee employee) {
-        return null;
+        return employeeRepository.save(requireNonNull(employee, "Null employee passed to save"));
     }
 
     @Override
     @Transactional
-    public Employee update(Integer id, Employee employee) {
-        return null;
+    public Optional<Employee> update(Integer id, Employee employee) {
+        Optional<Employee> updateCandidate;
+        if(id==null || employee==null){
+            return Optional.empty();
+        } else {
+            updateCandidate = employeeRepository.findById(id);
+            return updateEmployee(updateCandidate, employee);
+        }
+    }
+
+    private Optional<Employee> updateEmployee(Optional<Employee> updateCandidate, Employee employee) {
+        if(updateCandidate.isPresent()) {
+            Employee updatedEmployee = updateCandidate.get();
+            updatedEmployee
+                    .name(employee.name())
+                    .surname(employee.surname())
+                    .salary(employee.salary())
+                    .grade(employee.grade());
+        }
+        return updateCandidate;
     }
 
     @Override
     public CRUDEntityService<Employee, Integer> delete(Integer id) {
-        return null;
+        employeeRepository.deleteById(id);
+        return this;
     }
 }
