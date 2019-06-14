adb2eb1f7888f9a4d60b5c53c947262a915efdd6
Boris Victor <boristhevictor@gmail.com>
2019-06-14 10:10:59 +0200

    Modeled the fields for Employee, fixed failing tests. Closes #2

--- src/main/java/com/borysp/payments/uma/app/model/Employee.java
+++ src/main/java/com/borysp/payments/uma/app/model/Employee.java
@@ -1,7 +1,22 @@
 package com.borysp.payments.uma.app.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
 
+@SuppressWarnings("WeakerAccess")
 @Data
+@Accessors(fluent = true, chain = true)
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Employee {
+
+    private Integer id;
+    private int grade;
+    private int salary;
+    @NonNull private String name;
+    @NonNull private String surname;
+
 }
